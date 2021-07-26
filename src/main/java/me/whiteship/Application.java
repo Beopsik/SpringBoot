b5e081993b155c98f2c1ab98a49da571204a9f4d
BeobSik <tjswl756@naver.com>
2021-07-26 14:39:34 +0900

    feat: add autoconfiguration

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -1,6 +1,7 @@
 package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication application=new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
     }
 }
